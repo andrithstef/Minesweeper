08cb95e3b4bf5048d08565ac1b22387412873398
andrithstef <81168905+andrithstef@users.noreply.github.com>
2022-04-09 12:42:42 +0000

    Delete Minesweeper/Minesweeper directory

--- Minesweeper/Minesweeper/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.vidmot.minesweeper {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.minesweeper.vidmot to javafx.fxml;
-    exports com.minesweeper.vidmot;
-}
